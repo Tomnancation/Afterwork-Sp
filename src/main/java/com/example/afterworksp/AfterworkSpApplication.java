ea3ddd7f8488f6e4b16a9aa6dca8922e73ed83d1
Tomnancation <jita10752@eleve.isep.fr>
2024-12-13 22:45:41 +0100

    This is my frist sp projet

---
+++ src/main/java/com/example/afterworksp/AfterworkSpApplication.java
@@ -0,0 +1,13 @@
+package com.example.afterworksp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AfterworkSpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AfterworkSpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/afterworksp/AfterworkSpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.afterworksp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AfterworkSpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
